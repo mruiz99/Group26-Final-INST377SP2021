3af4f5a222dee645bcb04df7214c782026728683
augustmeyer <75350319+augustmeyer@users.noreply.github.com>
2021-04-15 22:22:08 -0400

    fixed spacing in developers.js

--- models/developers.js
+++ models/developers.js
@@ -1,19 +1,18 @@
 export default (sequelize, DataTypes) => {
-    const developers = sequelize.define(
-      'developers',
-      {
-        developer_team_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          primaryKey: true
-        },
-        developer_name: {
-          type: DataTypes.STRING, 
-          allowNull: false
-        },
+  const developers = sequelize.define(
+    'developers',
+    {
+      developer_team_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
       },
-      { freezeTableName: true, timestamps: false }
-    );
-    return developers;
-  };
-  
+      developer_name: {
+        type: DataTypes.STRING, 
+        allowNull: false
+      },
+    },
+    { freezeTableName: true, timestamps: false }
+  );
+  return developers;
+};
