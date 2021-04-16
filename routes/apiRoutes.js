74b49b0e0eea378fc621c0d869e731b4d1d58bad
Miguel <63415435+mruiz99@users.noreply.github.com>
2021-04-15 22:30:50 -0400

    making room for the other fellas

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import sequelize from 'sequelize';
+//import sequelize from 'sequelize';
 import db from '../database/initializeDB.js';
 
 const router = express.Router();
