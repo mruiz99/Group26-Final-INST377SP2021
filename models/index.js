2038f1fa20f635b238cc546d37f8303c9963557a
augustmeyer <75350319+augustmeyer@users.noreply.github.com>
2021-04-22 23:54:03 -0400

    Removing a model to be replaced

--- models/index.js
+++ models/index.js
@@ -1,13 +1,11 @@
 import games from './games.js';
 import genres from './genre.js';
-import developers from './developers.js';
 import platform from './platform.js';
 import publishers from './publishers.js';
 
 export default {
   games,
   genres,
-  developers,
   platform,
   publishers
 };
