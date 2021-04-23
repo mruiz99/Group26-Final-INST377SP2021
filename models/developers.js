dbf6f7a176a8db14494c6035b038ac80fa5dbb4f
Pete Schultz <pschultz010@gmail.com>
2021-04-23 00:11:26 -0400

    added developers model

---
+++ models/developers.js
@@ -0,0 +1,18 @@
+export default (sequelize, DataTypes) => {
+    const developers = sequelize.define(
+      'developers',
+      {
+        developer_team_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true
+        },
+        developer_name: {
+          type: DataTypes.STRING, 
+          allowNull: false
+        },
+      },
+      { freezeTableName: true, timestamps: false }
+    );
+    return developers;
+  };
--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ import games from './games.js';
 import genres from './genre.js';
 import platform from './platform.js';
 import publishers from './publishers.js';
+import developers from './developers.js';
 
 export default {
   games,
   genres,
   platform,
+  developers,
   publishers
 };
